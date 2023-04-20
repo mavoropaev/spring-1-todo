61dce5bcc5f705638beec23ca111a44196920a8c
mavoropaev <m.a.voropaev@gmail.com>
2023-04-20 13:06:58 +0300

    second

--- src/main/java/com/javarush/config/WebAppInitializer.java
+++ src/main/java/com/javarush/config/WebAppInitializer.java
@@ -1,2 +1,28 @@
-package com.javarush.config;public class WebAppInitializer {
+package com.javarush.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
